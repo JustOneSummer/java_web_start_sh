2589b9e27cea1efcb8281d0e502c7bb4b5b1bd36
JustOneSummer <myyuyukosama@gmail.com>
2023-04-11 23:58:32 +0800

    Create nohub-service.sh

---
+++ nohub-service.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# system path
+#SYSTEM_PATH=$(dirname $(readlink -f "$0"))
+SYSTEM_PATH="/root/proxy/glider_0.16.3_linux_amd64"
+CMD_STR="./glider -config $SYSTEM_PATH/config/glider.conf"
+echo "start service...."
+
+echo $SYSTEM_PATH
+
+pid_file="glider".pid
+
+function dev() {
+    $CMD_STR
+    service_start
+}
+
+function noHupLog() {
+    nohup $CMD_STR >"$SYSTEM_PATH"/log.log &
+    service_start
+}
+
+function noHupNoLog() {
+    nohup $CMD_STR >/dev/null 2>&1 &
+    service_start
+}
+
+function service_start() {
+    # shellcheck disable=SC2181
+    if [[ $? -eq 0 ]]; then
+        echo $! > ${pid_file}
+    else exit 1
+    fi
+}
+
+
+function service_stop() {
+    # shellcheck disable=SC2046
+    kill -9 $(cat ${pid_file})
+    # shellcheck disable=SC2181
+    if [[ $? -eq 0 ]]; then
+        rm -f ${pid_file}
+    else exit 1
+    fi
+    echo "java stop ok"
+}
+
+case "$1" in
+  dev)
+    dev
+    ;;
+  log)
+    noHupLog
+    ;;
+	start)
+		noHupNoLog
+		;;
+	stop)
+		java_stop
+		;;
+	status)
+		redis_status
+		;;
+	restart|reload)
+		service_stop
+		sleep 1
+		noHupNoLog
+		;;
+	*)
+		echo "Please use start or stop as first argument"
+		;;
+esac
